41aef88bb5ea27f507c5241d5308bbd87422ee14
Janosch Machowinski <janosch.machowinski@dfki.de>
2011-04-12 13:13:29 +0200

    ServoingTask: Recompute map after modification to it

--- tasks/ServoingTask.cpp
+++ tasks/ServoingTask.cpp
@@ -152,11 +152,13 @@ void ServoingTask::updateHook()
 	    double val = search_conf.robotWidth + search_conf.obstacleSafetyDistance + search_conf.stepDistance;
 	    //TODO calulate distance to laser beam inpackt based on laser angle
 	    mapGenerator.markUnknownInRectangeAsTraversable(curPose, val, val, 0.5);
+	    mapGenerator.computeNewMap();
 	    afterConfigure = false;
 	} 
 	else
 	{
 	    mapGenerator.markUnknownInRadiusAsObstacle(curPose, nearRadius);
+	    mapGenerator.computeNewMap();
 	}
 	
 	const TraversabilityGrid &trGridGMS(mapGenerator.getTraversabilityMap());
