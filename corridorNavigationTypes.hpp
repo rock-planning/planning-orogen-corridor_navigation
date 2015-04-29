18584f51017f5f8302aac095aeed87b1452fd638
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-04-29 11:20:44 +0200

    Removed FollowingTask
    The Corridor following has been unmaintained for over 2 years. The code
    was changed to make it compile, but it is very likley, that it does not
    even run any more. This also fixes the compile problem for gcc > 4.8

--- corridorNavigationTypes.hpp
+++ corridorNavigationTypes.hpp
@@ -1,38 +1,6 @@
 #ifndef OROGEN_CORRIDOR_NAVIGATION_TYPES__H
 #define OROGEN_CORRIDOR_NAVIGATION_TYPES__H
 
-#include <vfh_star/VFH.h>
-#include <vfh_star/TreeSearch.h>
-#include <base/float.h>
-#include <base/Pose.hpp>
-#include <base/Eigen.hpp>
-#include <vector>
-#include <corridor_planner/corridors.hh>
-
-
-namespace corridor_navigation {
-
-    struct TestConf {
-        std::vector< double > angular_windows;
-        double main_direction;
-    };
-
-    struct FollowingDebug {
-        base::Time planning_time;
-        base::Vector3d horizon[2];
-        vfh_star::DebugTree tree;
-    };
-
-    /** Type used to provide a complete problem to the task
-     */
-    struct CorridorFollowingProblem {
-        double desiredFinalHeading;
-        corridors::Corridor corridor;
-
-        CorridorFollowingProblem()
-            : desiredFinalHeading(base::unset<double>()) {}
-    };
-}
 
 #endif
 
